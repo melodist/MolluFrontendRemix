4ff8072aa02d1a73c874c1c47cf878b7d1f4dedb
melodist <melt.ep00@gmail.com>
2024-05-29 23:38:08 +0900

    Feat: 초기 레이아웃 생성

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -16,55 +16,57 @@ export default function Index() {
   return (
     <div className="container mx-auto">
       <div className="flex justify-center items-center">
-        <div className="grid justify-items-center content-center">
-          <div>
-            <button
-              type="button"
-              className="object-center px-1 py-1 flex justify-center items-center size-12 text-sm font-semibold rounded-full border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
-              onClick={play}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
+        <div className="box-border content-center h-screen">
+          <div className="grid gap-2 p-4 border-2 border-b-cyan-500 justify-items-center content-center">
+            <div>
+              <button
+                type="button"
+                className="object-center px-1 py-1 flex justify-center items-center size-12 text-sm font-semibold rounded-full border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                onClick={play}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div className="columns-3">
+              <div>
+                <button
+                  type="button"
+                  className="size-20 text-sm font-semibold rounded-full border border-transparent text-white bg-cover hover:ring hover:ring-blue-400 hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                  style={{
+                    backgroundImage: `url("images/portrait/example_1.png")`,
+                  }}
                 />
-              </svg>
-            </button>
-          </div>
-          <div className="columns-3">
-            <div>
-              <button
-                type="button"
-                className="size-20 text-sm font-semibold rounded-full border border-transparent text-white bg-cover hover:ring hover:ring-blue-400 hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
-                style={{
-                  backgroundImage: `url("images/portrait/example_1.png")`,
-                }}
-              />
-            </div>
-            <div>
-              <button
-                type="button"
-                className="size-20 text-sm font-semibold rounded-full border border-transparent text-white bg-cover hover:ring hover:ring-blue-400 hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
-                style={{
-                  backgroundImage: `url("images/portrait/example_2.png")`,
-                }}
-              />
-            </div>
-            <div>
-              <button
-                type="button"
-                className="size-20 text-sm font-semibold rounded-full border border-transparent text-white bg-cover hover:ring hover:ring-blue-400 hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
-                style={{
-                  backgroundImage: `url("images/portrait/example_3.png")`,
-                }}
-              />
+              </div>
+              <div>
+                <button
+                  type="button"
+                  className="size-20 text-sm font-semibold rounded-full border border-transparent text-white bg-cover hover:ring hover:ring-blue-400 hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                  style={{
+                    backgroundImage: `url("images/portrait/example_2.png")`,
+                  }}
+                />
+              </div>
+              <div>
+                <button
+                  type="button"
+                  className="size-20 text-sm font-semibold rounded-full border border-transparent text-white bg-cover hover:ring hover:ring-blue-400 hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                  style={{
+                    backgroundImage: `url("images/portrait/example_3.png")`,
+                  }}
+                />
+              </div>
             </div>
           </div>
         </div>
